21a0bbae941024c4141e5c209c0526d62cea27f8
TheScientist101 <66270347+TheScientist101@users.noreply.github.com>
2021-04-10 11:48:17 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import discord
+import requests
+import json
+import os
+from lxml import html
+from better_profanity import profanity
+from bill import insult
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="", case_insensitive=True)
+
+if not "TOKEN" in os.environ:
+    print("Please set the TOKEN environment variable.")
+    os._exit(1)
+
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send(
+        "Hello there!!! Psst, guys, this {0.author.mention} kid seems dumb.".format(
+            ctx.message
+        )
+    )
+
+
+@client.command()
+async def roast(ctx):
+    profanity.load_censor_words()
+    await ctx.send(profanity.censor("You are a " + insult()))
+
+
+@client.command()
+async def compliment(ctx):
+    profanity.load_censor_words()
+    await ctx.send(
+        profanity.censor(
+            [
+                e.text_content()
+                for e in html.fromstring(
+                    requests.get(
+                        "http://toykeeper.net/programs/mad/compliments",
+                    ).text
+                ).xpath("//h3")
+            ][0].replace("\n", "")
+        )
+    )
+
+
+@client.command()
+async def meme(ctx):
+    memejson = json.loads(requests.get("https://meme-api.herokuapp.com/gimme").text)
+    while memejson["nsfw"] == True:
+        memejson = json.loads(requests.get("https://meme-api.herokuapp.com/gimme").text)
+    imageUrl = memejson["url"]
+    embed = discord.Embed()
+    embed.set_image(url=imageUrl)
+    await ctx.send(embed=embed)
+
+
+client.run(os.environ["TOKEN"])
