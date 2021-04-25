e5ef59758484db899399242538d9ce15c8bc7b63
Urjith Mishra <urjithmishra@gmail.com>
2021-04-25 17:07:48 -0500

    typo

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if not "TOKEN" in os.environ:
 @client.event
 async def on_ready():
     print('Connected to bot: {}'.format(client.user.name))
-    print('Bot ID: {}'.format(client.user.id)))
+    print('Bot ID: {}'.format(client.user.id))
     client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='https://silly-bot.vercel.app/'))
 
 
